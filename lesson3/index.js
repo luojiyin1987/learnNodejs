0d84b492cab54f21af782b70775a646dcd65b47d
luojiyin <luojiyin@hotmail.com>
2018-08-26 10:36:40 +0800

    spider 2

--- lesson3/index.js
+++ lesson3/index.js
@@ -2,24 +2,39 @@ var cheerio = require('cheerio');
 var http = require('http');
 var iconv = require('iconv-lite');
 
-var url = 'http://www.ygdy8.net/html/gndy/dyzz/index.html';
+var index = 1;
+var url = 'http://www.ygdy8.net/html/gndy/dyzz/list_23_';
+var titles = [];
 
-http.get(url, function(sres) {
-    var chunks = [];
-    sres.on('data', function(chunk) {
-        chunks.push(chunk);
-    });
-
-    sres.on('end', function() {
-        var titles = []
-        var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
-        var $ =cheerio.load(html, {decodeEntities: false});
-        $('.co_content8 .ulink').each(function(idx, element){
-            var $element = $(element);
-            titles.push({
-                title: $element.text()
+function getTitle(url, i){
+    console.log("It is getting" + i + "content");
+    http.get(url + i + '.html', function(sres) {
+        var chunks = [];
+        sres.on('data', function(chunk){
+            chunks.push(chunk);
+        });
+        sres.on('end', function(){
+            var html = iconv.decode(Buffer.concat(chunks),'gb2312');
+            var $ = cheerio.load(html, {decodeEntities: false});
+            $('.co_content8 .ulink').each(function(idx, element){
+                var $element = $(element);
+                titles.push({
+                    title: $element.text()
+                })
             })
-        })
-        console.log(titles);
+            if(i < 2){
+                getTitle(url, ++index)
+            }else{
+                console.log(titles);
+                console.log("Title get finish");
+            }
+        });
     });
-})
+}
+
+function main() {
+    console.log("begin");
+    getTitle(url, index);
+}
+
+main();

b050a8a7cd6dbb2ee67d6921cbfc4002401341dc
luojiyin <luojiyin@hotmail.com>
2018-09-09 21:58:22 +0800

    md5

---
+++ lesson5/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var utility = require('utility');
+
+var app = express();
+
+app.get('/', function(req, res)
+{
+    var q = req.query.q;
+    if(!q)
+    {
+        res.send("no q");
+        return;
+    }
+    var md5Value = utility.md5(q);
+    res.send(md5Value);
+});
+
+app.listen(3000, function(req, res)
+{
+    console.log('app is running at port 3000');
+})

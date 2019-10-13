12a3a6035e002738a359f827880f2d1c21aa1f1a
luojiyin <luojiyin@hotmail.com>
2019-10-13 23:26:28 +0800

    finish

--- lesson9/app.js
+++ lesson9/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const wiki =  require('./route/wiki');
 const bodyParser = require('body-parser');
+const exec = require('child_process').exec;
 
 const app  = express();
 
@@ -23,11 +24,36 @@ app.get('/', (req, res)=>
 
 app.post('/download', (req, res)=>
 {
-   console.log("download", req.body);
+   console.log("download", req.body.downloadURL);
+   const {downloadURL} = req.body;
+
+   if(isEmpty(downloadURL) || isBlank(downloadURL))
+   {
+       res.json({result:1 , err: "no url"});
+       return;
+   }
    res.json({result:0, download:"ok"});
+   exec(`youtube-dl ${downloadURL}`,
+       (err, rtn) =>
+       {
+           console.log("youtube-dl", err, rtn);
+       });
+
+
+
 });
 
 app.listen(3000, ()=>
 {
     console.log('app run at port 3000');
 });
+
+function   isEmpty(str)
+{
+    return (!str || 0 === str.length);
+}
+
+function isBlank(str)
+{
+    return (!str || /^\s*$/.test(str));
+}

1e3b235047d4b29e1db92f12e8cf3cd3a9df5bfc
luojiyin <luojiyin@hotmail.com>
2019-10-13 12:22:22 +0800

    use   http module

--- lesson9/src/app.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Button from './button'
-
-Vue.component('g-button', Button)
-
-new Vue({
-    el: '#app',
-})
---
+++ lesson9/test-node.js
@@ -0,0 +1,9 @@
+const  http = require("http");
+const  port = 8000;
+http.createServer((req, res)=>
+{
+    res.writeHead(200, {'Content-Type':  'text/plain'});
+    res.end('Hellow World\n');
+}).listen(port);
+
+console.log('server run at  port  8000')

ba2574324427a7adebef1b711de35f9137e969be
luojiyin <luojiyin@hotmail.com>
2019-10-13 22:47:37 +0800

    post req.body

--- lesson9/app.js
+++ lesson9/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const wiki =  require('./route/wiki');
+const bodyParser = require('body-parser');
+
 const app  = express();
+
+app.use(bodyParser.json());
+
 
 app.use('/wiki', wiki);
 
@@ -16,6 +21,11 @@ app.get('/', (req, res)=>
     res.sendfile('index.html');
 });
 
+app.post('/download', (req, res)=>
+{
+   console.log("download", req.body);
+   res.json({result:0, download:"ok"});
+});
 
 app.listen(3000, ()=>
 {
